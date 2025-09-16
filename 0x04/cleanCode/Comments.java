6498f29b9ffd819424e4356dd0c51ee2400b7eb0
Helcio Fernando Ueda <helcio.ueda@bradesco.com.br>
2025-09-16 07:21:01 -0300

    04_cleancode

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+
+    // TODO Este método deve ser removido
+    public void check() {
+        return;
+    }
+
+    /**
+     * <p>Este método publica resultados com base na data.</p>
+     *
+     * @param date data a partir da qual os resultados devem ser publicados
+     * @return Lista de entidades de resultado
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // alguma lógica de negócios aqui
+    }
+}
